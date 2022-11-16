5169d3bd6adf57597ce3adf759bca869232db7ad
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-11-16 10:38:18 +0000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Statistics } from './Statistics';
 import { FeedbackOptions } from './FeedbackOptions';
 import { Section } from './Section';
